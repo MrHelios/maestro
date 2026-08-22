0a548a73832cfa7b01a144844f7f5f0e4ddcb412
Luciano <lhitaliani@gmail.com>
2026-08-22 09:12:55 -0300

    Ahora se puede arrancar el editor si necesidad de abrir un archivo por defecto

--- ui/main.cpp
+++ ui/main.cpp
@@ -2,21 +2,21 @@
 #include "ui/Editor.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::fprintf(stderr, "Uso: %s <archivo>\n", argv[0]);
-        return 1;
-    }
-
-    if (Editor::isDirectory(argv[1])) {
-        std::fprintf(stderr,
-                     "Error: '%s' es una carpeta. Solo se pueden abrir archivos.\n",
-                     argv[1]);
-        return 1;
-    }
-
     Editor editor;
-    editor.openFile(argv[1]);
-    editor.run();
 
+    // Sin argumentos: arranca con el buffer vacío "SinNombre" que ya
+    // crea BufferManager en su constructor. Con un path: lo abre (o crea
+    // en memoria si no existe).
+    if (argc >= 2) {
+        if (Editor::isDirectory(argv[1])) {
+            std::fprintf(stderr,
+                         "Error: '%s' es una carpeta. Solo se pueden abrir archivos.\n",
+                         argv[1]);
+            return 1;
+        }
+        editor.openFile(argv[1]);
+    }
+
+    editor.run();
     return 0;
 }
